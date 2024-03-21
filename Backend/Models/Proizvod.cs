01377a1574a06c9d638f9a63fe55fa886284e3d4
Ivan Marosevic <ivanmarosevic4@gmail.com>
2024-03-21 18:46:23 +0100

    Backend

---
+++ Backend/Models/Proizvod.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Proizvod: Entitet
+    {
+        public string? Naziv { get; set; }
+        public int? Proizvodjac { get; set; }
+        
+
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,41 @@
+
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<TekucineContext>(o => {
+    o.UseSqlServer(builder.Configuration.GetConnectionString("TekucineContext"));
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.MapFallbackToFile("index.html");
+
+
+app.Run();
