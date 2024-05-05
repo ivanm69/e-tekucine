2026392a2a2582d650cb37c26c1ce1a0310da0ec
Ivan Marosevic <ivanmarosevic4@gmail.com>
2024-05-05 16:51:14 +0200

    Frontend u produkciji

--- Frontend/src/pages/NadzornaPloca.jsx
+++ Frontend/src/pages/NadzornaPloca.jsx
@@ -1,71 +1,81 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Carousel } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import chuckImage from '../assets/chuck.png';
+import desertImage from '../assets/desert.jpg';
+import duhanskaImage from '../assets/Duhanska.jpg';
+import karatekidImage from '../assets/karatekid.png';
+import voceImage from '../assets/voce.jpg';
+import mentholImage from '../assets/Menthol.jpg';
 
-
-const CarouselComponent = () => {
+var heroData = [
+  {
+    id: 1,
+    image: chuckImage,
+    title: 'Chuck Norris',
+    description: 'Uziva u našoj duhanskoj tekućini.',
+    
+  },
+  {
+    id: 2,
+    image: voceImage,
+    title: 'Voćna Aroma',
+     
+    
+  },
+  {
+    id: 3,
+    image: duhanskaImage,
+    title: 'Duhanska Aroma',
+    
+    
+  },
+  {
+    id: 4,
+    image: desertImage,
+    title: 'Desertna Aroma',
+   
+    
+  },
+  {
+    id: 5,
+    image: mentholImage,
+    title: 'Menthol Aroma',
+    
+    
+  },
+  {
+    id: 6,
+    image: karatekidImage,
+    title: 'Otkrijte sve vrste aroma na našoj stranici',
+    
+    
+  },
+]
+function AppHero() {
   return (
-    <Container className='mt-4'>
-     <Carousel >
-        <Carousel.Item>
-          <img
-            className="d-block w-100 h-50"
-            src="src/assets/chuck.png"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3 class="text-cyan-50 font-serif text-xl">Chuck Norris</h3>
-            <p class="text-cyan-50 font-serif text-xl">Uziva u našoj duhanskoj tekućini.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="src/assets/voce.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3 class="text-cyan-50 font-serif text-xl">Voćna Aroma</h3>
-            
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="src/assets/desert.jpg"
-            alt="Third slide"
-          />
-           <Carousel.Caption>
-            <h3 class="text-cyan-50 font-serif text-xl">Desertna Aroma</h3>
-            
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="src/assets/Duhanskajpg.jpg"
-            alt="Fourth slide"
-          />
-          <Carousel.Caption>
-            <h3 class="text-cyan-50 font-serif text-xl">Duhanska Aroma</h3>
-            
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="src/assets/karatekid.png"
-            alt="Fifth slide"
-          />
-          <Carousel.Caption>
-            <h3 class="text-cyan-50 font-serif text-xl">Otkrijte sve vrste aroma na našoj stranici </h3>
-            
-          </Carousel.Caption>
-
-        </Carousel.Item>
+    
+    <section id="home" className="mt-4">
+       <Carousel>
+          {
+            heroData.map(hero => {
+              return (
+                <Carousel.Item key={hero.id} className='hero-item' >
+                  <img
+                    className="d-block  w-100 hero-image "
+                    src={hero.image}
+                    alt={"slide " + hero.id}
+                  />
+                  <Carousel.Caption>
+                    <h2 class="text-cyan-50 font-serif text-xl">{hero.title}</h2>
+                    <p class="text-cyan-50 font-serif ">{hero.description}</p>
+                    
+                  </Carousel.Caption>             
+                </Carousel.Item>
+              );
+            })
+          }
       </Carousel>
-    </Container>
+    </section>
   );
-};
+}
 
-export default CarouselComponent;
+export default AppHero;
